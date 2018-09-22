e1205230e224973f5c9e6003fe712effeaf06279
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-23 02:00:53 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'prime'
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def two_dinosaurs
@@ -14,7 +15,6 @@ class ApplicationController < ActionController::Base
     s1[0] = 1
     s2[0] = 1
     s=0
-    beginning_time = Time.now
     for j in 0..n-1 do
       s+=array1[j].to_i
       i = s
@@ -23,7 +23,6 @@ class ApplicationController < ActionController::Base
         i = i - 1
       end
     end
-    end_time = Time.now
     t=0
     for k in 0..n-1 do
       t+=array2[k].to_i
@@ -33,17 +32,35 @@ class ApplicationController < ActionController::Base
         i = i - 1
       end
     end
-    beginning_time = Time.now
     summation_array1 = Array.new(sum1+2, 0)
     summation_array1.each.with_index do |_, index|
       if index != 0
         summation_array1[index] = s1[index-1] + summation_array1[index-1]
       end
     end
-    end_time = Time.now
     result = s2.map.with_index do |el, index|
       (el * (summation_array1[[sum1+1, index+d+1].min] - summation_array1[[0, [index - d, sum1 + 1].min].max]+mod_val)%mod_val)%mod_val
     end.inject(0, :+)
     render json: {"result": (result % mod_val) }
+  end
+
+  def prime_sum
+    render json: {"output": find_prime_component(params["input"])}
+  end
+
+  private
+
+  def find_prime_component(x)
+    for z in (x).downto(0)
+      if Prime.prime?(z)
+        if z == x
+          return [x]
+        elsif z == x - 1
+          next
+        else
+          return find_prime_component(x - z).unshift(z)
+        end
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post '/two-dinosaurs' => 'application#two_dinosaurs'
+  post '/prime-sum' => 'application#prime_sum'
 end
