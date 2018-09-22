9a8c52ab1551a0f2554ebf679c58e83dc933b7ee
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 15:36:23 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require 'json'
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   def two_dinosaurs(jsonPayload)
     puts JSON.pretty_generate(jsonPayload)
     return {result => 8}
