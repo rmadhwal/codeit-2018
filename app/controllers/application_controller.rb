550d6da8c72606f98887e72f3b656cbfb0dfd6c3
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 15:46:06 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ require 'json'
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def two_dinosaurs
-    puts JSON.pretty_generate(params)
+    n = params["number_of_types_of_food"]
+    r = params["calories_for_each_type_for_raphael"]
+    l = params["calories_for_each_type_for_leonardo"]
+    d = params["maximum_difference_for_calories"]
+    puts("n" + n)
+    puts("r" + r)
+    puts("l" + l)
+    puts("d" + d)
     return {"result": 8}
   end
 end
