2d76ad0375a647f2731ef2dbcdb17bc48deb7b27
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 20:02:24 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,22 +10,26 @@ class ApplicationController < ActionController::Base
     sum2 = array2.inject(0, :+)
     s1 = Array.new(sum1+1, 0)
     s2 = Array.new(sum2+1, 0)
-    for i in 0..(2**n-1) do
-      sum = i.to_s(2).rjust(n, "0").split("").map.with_index do |el, index|
-        el == "1" ? array1[index] : 0
-      end.inject(0, :+)
-      s1[sum] += 1
+    for j in 0..n-1 do
+      s+=array1[j].to_i
+      i = s
+      while i>=array1[j].to_i do
+        s1[i]=(s1[i]+s1[i-array1[j].to_i])
+        i = i - 1
+      end
     end
-    for i in 0..(2**n-1) do
-      sum = i.to_s(2).rjust(n, "0").split("").map.with_index do |el, index|
-        el == "1" ? array2[index] : 0
-      end.inject(0, :+)
-      s2[sum] += 1
+    for k in 0..n-1 do
+      t+=array2[k].to_i
+      i = t
+      while i>=array2[k].to_i do
+        s2[i]=(s2[i]+s2[i-array2[k].to_i])
+        i = i - 1
+      end
     end
     result = s1.map.with_index do |el, index|
       total = 0
       for i in (-1*d)..d
-        total += s2[index+i] if index+i>=0
+        total += s2[index+i] if (index+i>=0 && index+i<n)
       end
       el*total
     end.inject(0, :+)
