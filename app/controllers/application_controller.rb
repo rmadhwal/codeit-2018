d06771388b79b43dfdf1ca07ec35e26ae4a358db
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 15:52:45 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,32 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def two_dinosaurs
     n = params["number_of_types_of_food"]
-    r = params["calories_for_each_type_for_raphael"]
-    l = params["calories_for_each_type_for_leonardo"]
+    array1 = params["calories_for_each_type_for_raphael"]
+    array2 = params["calories_for_each_type_for_leonardo"]
     d = params["maximum_difference_for_calories"]
-    puts("n" + JSON.pretty_generate(n))
-    puts("r" + JSON.pretty_generate(r))
-    puts("l" + JSON.pretty_generate(l))
-    puts("d" + JSON.pretty_generate(d))
-    return {"result": 8}
+    number_of_ways = 0
+    number_of_variations = (2**n)-1
+    for x in 0..number_of_variations do
+      suma = 0
+      y = x.to_s(2).rjust(n, "0")
+      z = array1.each.with_index do |el, index|
+        if y[index] == '1'
+          suma = suma + el.to_i
+        end
+      end
+      for x2 in 0..number_of_variations do
+        sumb = 0
+        y2 = x2.to_s(2).rjust(n, "0")
+        z2 = array2.each.with_index do |el, index|
+          if y2[index] == '1'
+            sumb = sumb + el.to_i
+          end
+        end
+        if (suma-sumb).abs <= d
+          number_of_ways = number_of_ways + 1
+        end
+      end
+    end
+    return {"result": ((number_of_ways) % 100000123) }
   end
 end
