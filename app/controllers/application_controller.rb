11f31876b05ecb87f45d281b88cfb68af68dc607
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 17:19:16 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def two_dinosaurs
     n = params["number_of_types_of_food"]
-    array1 = JSON.parse(params["calories_for_each_type_for_raphael"])
-    array2 = JSON.parse(params["calories_for_each_type_for_leonardo"])
+    array1 = params["calories_for_each_type_for_raphael"]
+    array2 = params["calories_for_each_type_for_leonardo"]
     d = params["maximum_difference_for_calories"]
     puts n.to_s
     puts array1.class.name
