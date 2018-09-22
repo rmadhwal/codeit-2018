d7b1b900f76d6208771a41d3825fa30ef90a53c8
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 15:42:24 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   def two_dinosaurs
     puts JSON.pretty_generate(params)
-    return {result => 8}
+    return {"result": 8}
   end
 end
