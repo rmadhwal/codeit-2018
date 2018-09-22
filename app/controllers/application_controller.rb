87682d161e2be1489e9939cc3c805e9f76d02e4d
rmadhwal <rohanmadhwal@yahoo.co.in>
2018-09-22 15:34:19 +0800

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ require 'json'
 class ApplicationController < ActionController::Base
   def two_dinosaurs(jsonPayload)
     puts JSON.pretty_generate(jsonPayload)
+    return {result => 8}
   end
 end
